557637cbb30b0bfff3137d16fff2ff14ab3dbf05
Axel Emanuel <100535827+axelemanuel23@users.noreply.github.com>
2025-09-05 16:29:39 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -458,13 +458,13 @@ ARTÍCULO 1°.- Autorízase el ingreso sin visado consular argentino ni Autoriza
 POPULAR DE CHINA, titulares de pasaporte ordinario, cuando el mismo se produzca con la presentación de visado válido y vigente para el ingreso a los
 ESTADOS UNIDOS DE AMÉRICA correspondiente a similar categoría migratoria.
 En el caso de los nacionales de la REPÚBLICA POPULAR DE CHINA, resultarán comprendidos en el referido beneficio las subcategorías migratorias del
-artículo 24 de la Ley N° 25.871, inciso a) “turistas” y h) Disposición N° 1171/10 “hombre de negocios”, con un plazo de permanencia máximo autorizado
+artículo 24 de la Ley N° 25.871, inciso a) 'turistas' y h) Disposición N° 1171/10 'hombre de negocios', con un plazo de permanencia máximo autorizado
 de hasta TREINTA (30) días." },
   { pais: "", codigo: "IND", texto: "Mediante RESOL-2025-353-APN-VGI#JGM-Resolución N° 353 del 26 de agosto de 2025 (entra en vigencia a partir del 28/07/2025), se resuelve:
 ARTÍCULO 1°.- Autorízase el ingreso sin visado consular argentino ni Autorización de Viaje Electrónica (AVE), a los extranjeros nacionales de la REPÚBLICA DE LA
 INDIA, titulares de pasaporte ordinario, cuando el mismo se produzca con la presentación de visado válido y vigente para el ingreso a los ESTADOS UNIDOS DE
 AMÉRICA correspondiente a similar categoría migratoria, previa verificación de las mismas con las autoridades pertinentes y su ingreso al país se efectuare en
-carácter transitorio en los términos del inciso a) “Turistas” del artículo 24 de la Ley N° 25.871 y sus modificatorios, con un plazo de hasta NOVENTA (90) días de
+carácter transitorio en los términos del inciso a) 'Turistas' del artículo 24 de la Ley N° 25.871 y sus modificatorios, con un plazo de hasta NOVENTA (90) días de
 permanencia." },
 
     
